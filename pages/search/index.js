ec63dbb14c261b7c6363cbb6aac3075a98094d7a
Isaac Faulkner <iwf2@nau.edu>
2023-04-30 21:16:12 -0700

    cleaned up fix nav bar

--- pages/search/index.js
+++ pages/search/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
 import { useEffect } from 'react';
 import { setProfile } from '/functions/profile-display.js';
-import NavBar from '../functions/navBar-display.js';
+import NavBar from '../../functions/navBar-display.js';
 
 
 
@@ -19,23 +19,6 @@ export default function Search() {
       </Head>
 
       <NavBar/>
-
-      <div className="tab-container">
-        <button className="toggle-button" onClick={toggleVisibility}>
-          {isVisible ? '| | |' : '| | |'}
-        </button>
-        <nav className={`navbar ${isVisible ? 'visible' : ''}`}>
-          <ul className="list">
-            <li><a href ="../">Home</a></li>
-            <li><a href ="playlists">Playlists</a></li>
-            <li><a href ="account-creation">creat an account</a></li>
-            <li><a href ="profile">profile</a></li>
-            <li><a href ="login">login</a></li>
-            <li><a href ="search">search</a></li>
-            <img className='icon' src="/icon.png" alt='icon'/>
-          </ul>
-        </nav>
-     </div>
 
       <main>
       <a id="userBox" hidden href="/profile" className={styles.userBox}>
@@ -79,127 +62,7 @@ export default function Search() {
 
       <style jsx>{`
 
-      .tab-container {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 50px;
-      }
-
-      .toggle-button {
-        position: fixed;
-        top: 10px;
-        left: 0;
-        margin: 10px;
-        cursor: pointer;
-        z-index: 1;
-        
-        
-        
-      }
-
-      .toggle-button:hover {
-        background-color: #3e8e41;
-      }
-
-
-
-      button {
-        background-color: #4CAF50;
-        color: #fff;
-        padding: 10px 10px;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        font-size: 15px;
-        margin-top: 20px;
-        
-      }
-
-      .navbar{
-        top: -100px;
-        left: 0;
-        width: 100%;
-        color: white;
-        background-color: black;
-        position: fixed;
-        padding: 10px;
-        transition: top .5s ease-in-out;
-        
-      }
-
-      .navbar.visible{
-        top: 0;
-      }
-
-      li{
-        float: left;
-        padding-right: 50px;
-      }
-
-      li a:hover{
-        color: #4CAF50;
-        text-decoration: underline;
-      }
-
-      li a {
-        
-        width: 100%;
-        display: block;
-        text-decoration: none;
-        color: #fff;
-        text-transform: uppercase;
-        font-weight: bold;
-        
-      }
-
-      .list {
-        color: black;
-        padding-bottom: 20px;
-        padding-left: 90px;
-
-      }
-
-      .list li:hover {
-        animation: anim2 1s cubic-bezier(0.175, 0.885, 0.32, 0.275) 1;
-        list-style: none;  
-        
-      }
-      
-      @keyframes anim2 {
-        0% {
-          opacity: 1;
-          transform: translateY(0px);
-          
-        }
-        25% {
-          opacity: 0;
-          transform: translateY(-10px);
-        }
-        50% {
-          opacity: 1;
-          transform: translateY(0px);
-        }
-        75% {
-          opacity: 0;
-          transform: translateY(10px);
-        }
-        100% {
-          opacity: 1;
-          transform: translateY(0px);
-        }
-      }
-
-      .icon{
-        width: 30px;
-        height: 30px;
-        float: right;
-
-      }
-
-
-        main {
+          main {
           padding: 5rem 0;
           flex: 1;
           display: flex;
